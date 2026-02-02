a5918ca277bb701240dea59730ee356e7d0b01a5
Muhammad Mohsin <mmohsinnaseer7@gmail.com>
2026-02-02 16:45:36 +0500

    Update Files

---
+++ Sources/CalculatorPackage/CalculatorPackage.swift
@@ -0,0 +1,17 @@
+public struct Calculator
+{
+
+    public init() { }
+
+    public func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+
+    public func minus(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+
+    public func multiply(_ a: Int, _ b: Int) -> Int {
+        return a * b
+    }
+}
