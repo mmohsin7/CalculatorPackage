058f6949f7f765b74554a1a7958b644f038a5f99
Muhammad Mohsin <mmohsinnaseer7@gmail.com>
2026-02-05 10:41:22 +0500

    commit

--- Sources/CalculatorPackage/CalculatorPackage.swift
+++ Sources/CalculatorPackage/CalculatorPackage.swift
@@ -8,12 +8,24 @@ public struct PackageView: View
     {
         VStack
         {
+            // MARK: For 1.0
             Text("Package View Text 1.0")
             Button("Package View Button 1.0")
             {
                 print("Package View Button 1.0 Clicked")
             }
+            
+            // MARK: For 2.0
+            Text("Package View Text 2.0")
+            Button("Package View Button 2.0")
+            {
+                print("Package View Button 2.0 Clicked")
+            }
         }
         .padding()
     }
+}
+
+#Preview {
+    PackageView()
 }
